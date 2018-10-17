fa0bcb925d7bbe4eca40601b8a358f26fa616d66
Jack Bogart <bogart.jack98@gmail.com>
2018-10-17 11:14:39 -0600

    basic building blocks

---
+++ db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://localhost/fruit';
+
+mongoose.connect(connectionString, {useNewUrlParser: true});
+
+mongoose.connection.on('connected', ()=>{
+	console.log('mongoose connection established to: '+connectionString);
+});
+
+mongoose.connection.on('disconnected', ()=>{
+	console.log('mongoose connection failed');
+});
+
+mongoose.connection.on('error', (err)=>{
+	console.log('mongoose connection error: '+err);
+});
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+require('./db/db.js');
+const app = express();
+const vampireController = require('./controllers/vampires');
+
+app.use(bodyParser.urlencoded({extended: false});
+app.use(methodOverride('_method');
+app.use(
+
